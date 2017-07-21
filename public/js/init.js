fc8860232e125a54a6ec00b72d40841ed06a7bdd
rjayacaba <acabarjayit1b@gmail.com>
2017-07-21 22:32:51 +0800

    first commit

---
+++ public/js/init.js
@@ -0,0 +1,13 @@
+$(".button-collapse").sideNav();
+
+$(".dropdown-button").dropdown();
+
+$(document).ready(function(){
+      $('.parallax').parallax();
+});
+
+
+$(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+});
+      
