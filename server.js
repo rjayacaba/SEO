1ac3555caff32bde93e5c697d34c4fa3f44c0edb
rjayacaba <acabarjayit1b@gmail.com>
2017-10-20 02:52:16 +0800

    deploy to heroku

--- server.js
+++ server.js
@@ -24,6 +24,21 @@ app.get('/use-of-tags-and-backlinking', function(req, res){
   res.sendFile(path.join(__dirname, 'views/tags.html'));
 });
 
+app.get('/organic-traffic', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/organic-traffic.html'));
+});
+app.get('/article7', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/article7.html'));
+});
+app.get('/website-needs-seo', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/article8.html'));
+});
+app.get('/seo-ethics', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/article9.html'));
+});
+app.get('/article10', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/article10.html'));
+});
 
 //express server listen
 var server = app.listen(app.get('port'), function(){
